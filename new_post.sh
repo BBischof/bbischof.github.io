5fd3b32c8ef05de1100807133633602cb02e817d
bbischof <bryan.bischof@gmail.com>
2017-05-19 16:39:14 -0700

    Update newpost script and puzzle blog

--- new_post.sh
+++ new_post.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-subl _posts/blog/$(date  +%Y-%m-%d)-$1.md
+name=$1
+destination='_posts/blog/'$(date  +%Y-%m-%d)'-'$name'.md'
+touch $destination
+name=`echo ${name:0:1} | tr  '[a-z]' '[A-Z]'`${name:1}
+header='---\n layout: post\n title: "'$name'"\n modified:\n categories: blog\n excerpt:\n tags: []\n image:\n   feature:\n date: '$(date  +%Y-%m-%d)'T00:00:01-04:00\n---\n\n\n<!-- ^Spoiler Text^ -->\n##'
+echo $header > $destination
+subl $destination
